cf484113337ccc5f018c726bab1a75151a099416
User01 <user01@uds-507824.ad.unistra.fr>
2023-09-19 08:44:14 +0200

    test arg number & convert and check port number

--- 01-emetteurUDPv4/sender-udp.c
+++ 01-emetteurUDPv4/sender-udp.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <inttypes.h>
 
 #define CHECK(op)   do { if ( (op) == -1) { perror (#op); exit (EXIT_FAILURE); } \
                     } while (0)
@@ -14,8 +15,22 @@
 int main (int argc, char *argv [])
 {
     /* test arg number */
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./sender-udp port_number\n");
+        exit(EXIT_FAILURE);
+    }
 
     /* convert and check port number */
+    int port_number = atoi(argv[1]);
+    if (port_number == 0){
+        fprintf(stderr, "erreur atoi\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!(port_number >= 10000 && port_number <= 65000)){
+        fprintf(stderr, "Numero de port doit etre contenu dans l'intervalle [10000, 65000]\n");
+        exit(EXIT_FAILURE);
+    }
 
     /* create socket */
 
