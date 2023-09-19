ebf0221bb1fcf13d4cdfc5489f6d8b42fb58b1f3
User01 <user01@uds-507824.ad.unistra.fr>
2023-09-19 09:39:03 +0200

    test arg, convert check port number, socket creation

--- 02-config-adresse/sender-udp.c
+++ 02-config-adresse/sender-udp.c
@@ -14,10 +14,26 @@
 int main (int argc, char *argv [])
 {
     /* test arg number */
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./sender-udp port_number\n");
+        exit(EXIT_FAILURE);
+    }
 
     /* convert and check port number */
+    int port_number = atoi(argv[1]);
+    if (port_number == 0){
+        fprintf(stderr, "erreur atoi\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!(port_number >= 10000 && port_number <= 65000)){
+        fprintf(stderr, "Numero de port doit etre contenu dans l'intervalle [10000, 65000]\n");
+        exit(EXIT_FAILURE);
+    }
 
     /* create socket */
+    int udp_socket;
+    CHECK(udp_socket = socket(AF_INET, SOCK_DGRAM, 0));
 
     /* complete sockaddr struct */
 
