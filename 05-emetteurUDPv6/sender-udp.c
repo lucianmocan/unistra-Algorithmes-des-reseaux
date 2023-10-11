ee339b81c6d865612b6840d2bc71d200f5fa778d
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-10-11 19:40:17 +0200

    arg number and port check

--- 05-emetteurUDPv6/sender-udp.c
+++ 05-emetteurUDPv6/sender-udp.c
@@ -1,6 +1,7 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -11,11 +12,34 @@
 
 #define IP "::1"
 
+void usage(char* message){
+    fprintf(stderr, "Usage: ./sebder-udp port_number");
+    fprintf(stderr, "%s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+long cook_port_number(char* str_port, int* int_port){
+    char* endptr;
+    long port = strtol(str_port, &endptr, 10); 
+    if (!(strcmp(endptr, "\0") == 0) || !(port >= 10000 && port <= 65000)){
+        return -1;
+    }
+    *int_port = port;
+    return 0;
+}
+
 int main (int argc, char *argv [])
 {
     /* test arg number */
+    if (argc != 2){
+        usage("");
+    }
 
     /* convert and check port number */
+    int port_number;
+    if(cook_port_number(argv[1], &port_number) == -1){
+        usage("10000 <= port_number <= 65000");
+    };
 
     /* create socket */
 
