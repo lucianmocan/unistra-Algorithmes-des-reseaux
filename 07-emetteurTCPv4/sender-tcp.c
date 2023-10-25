c30eea385d1176fc7f25f5d34f9aab7295055e67
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-10-25 10:28:17 +0200

    create TCP socket and use SO_REUSEADDR

--- 07-emetteurTCPv4/sender-tcp.c
+++ 07-emetteurTCPv4/sender-tcp.c
@@ -6,19 +6,54 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <errno.h>
 
 #define CHECK(op)   do { if ( (op) == -1) { perror (#op); exit (EXIT_FAILURE); } \
                     } while (0)
 
 #define IP "127.0.0.1"
 
+void usage(){
+    fprintf(stderr, "usage: ./receiver-tcp ip_addr port_number");
+    exit(EXIT_FAILURE);
+}
+long cook_port_number(char* str_port, int* int_port){
+    char* endptr;
+    long port = strtol(str_port, &endptr, 10); 
+    if (errno){
+        perror("strtol error");
+        exit(EXIT_FAILURE);
+    }
+    if (!(strcmp(endptr, "\0") == 0) || 
+        !(port >= 10000 && port <= 65000)){
+        return -1;
+    }
+    *int_port = port;
+    return 0;
+}
+
 int main (int argc, char *argv [])
 {
     /* test arg number */
+    if (argc != 3){
+        usage();
+    }
 
     /* convert and check port number */
-
+    int port_number;
+    if(cook_port_number(argv[2], &port_number) == -1){
+        usage();
+    };
+    char* ip_address = argv[1];
+    char* str_port_number = argv[2];
+    
     /* create socket */
+    int tcp_socket;
+    CHECK(tcp_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP));
+
+    /* SO_REUSEADDR option allows re-starting the program without delay */
+    int iSetOption = 1;
+    CHECK (setsockopt (tcp_socket, SOL_SOCKET, SO_REUSEADDR, &iSetOption, sizeof iSetOption));
 
     /* complete struct sockaddr */
 
