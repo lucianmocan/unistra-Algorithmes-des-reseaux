eb05ab77ea7813c46cd687edb8a91db5d7b23d04
Julien Montavont <montavont@unistra.fr>
2023-11-11 18:16:09 +0100

    new tests

--- 10-transfert-fichierTCPv6-dest/README.md
+++ 10-transfert-fichierTCPv6-dest/README.md
@@ -4,9 +4,9 @@
 
 Complétez le programme `receiver-tcp.c` pour recevoir le contenu d'un fichier avec les protocoles IPv6 et TCP.
 
-Le programme `receiver-tcp` admet en argument le numéro de port sur lequel le programme doit écouter :
+Le programme `receiver-tcp` admet en argument l'adresse IP et le numéro de port sur lequel le programme doit écouter :
 
-    ./receiver-tcp port_number
+    ./receiver-tcp ip_addr port_number
 
 Ce programme devra copier les données reçues sur le socket dans le fichier `copy.tmp` qui devra être créé dans le répertoire courant.
 
@@ -16,13 +16,13 @@ Les seuls numéros de port valides sont ceux contenus dans l'intervalle `[10000;
 
 ## Marche à suivre
 
-Vous pouvez reprendre une grande partie du code produit pour l'exercice 8. Vous devez simplement mettre à jour les parties spécifiques à IPv4. Le programme `receiver-tcp` devra écouter sur l'adresse `::1` et le port passé en argument du programme.
+Vous pouvez reprendre une grande partie du code produit pour l'exercice 8. Vous devez simplement mettre à jour les parties spécifiques à IPv4. Le programme `receiver-tcp` devra écouter sur l'adresse IPv6 et le port passé en argument du programme.
 
-La copie (depuis le socket vers le fichier `copy.tmp` pour le destinataire) devra être réalisée par la fonction à compléter :
+La copie (depuis le socket vers le fichier `copy.tmp`) devra être réalisée par la fonction à compléter :
 
     void cpy (int in, int out)
 
-où `in` et `out` sont respectivement les descripteurs sur lesquels lire et écrire.
+où `in` et `out` sont respectivement les descripteurs sur lesquels lire et écrire. Vous veillerez à lire sur le socket par blocs d'octets pour limiter le nombre d'appels système.
 
 N'oubliez pas d'ajouter le code suivant après la création du socket `sockfd` pour les raisons évoquées dans l'exercice 8 :
 
@@ -32,7 +32,7 @@ N'oubliez pas d'ajouter le code suivant après la création du socket `sockfd` p
 
 Testez votre programme à l'aide du programme réalisé dans l'exercice précédent, ou via la commande :
 
-    nc -6 ::1 port_de_votre_programme < fichier_a_transmettre
+    nc -6 adresse_ip_de_votre_programme  port_de_votre_programme < fichier_a_transmettre
 
 Vérifier que le fichier source et sa copie `copy.tmp` sont identiques via la commande `cmp` :
 
--- 10-transfert-fichierTCPv6-dest/tests.sh
+++ 10-transfert-fichierTCPv6-dest/tests.sh
@@ -1,65 +1,157 @@
 #!/bin/bash
 
 PROG="./receiver-tcp"
+FILE="$PROG.c"
+
 PORT=`shuf -i 10000-65000 -n 1`
 
-echo -n "test 01 - program without arg: "
-$PROG > /dev/null 2>&1 && echo "KO -> exit status $? instead of 1" && exit 1 
-echo "....................OK"
+OUT="/tmp/$$"
+mkdir $OUT
 
-echo -n "test 02 - invalid port number: "
-$PROG 65001 > /dev/null 2>&1 && echo "KO -> exit status $? instead of 1" && exit 1 
-echo "....................OK"
+IP="::1"
 
-echo -n "test 03 - program exits without error: "
-timeout 5 $PROG $PORT 2> /dev/null &
+######################################
+
+echo -n "test 01 - program usage: "
+
+echo "usage: $PROG ip_addr port_number" > $OUT/usage
+$PROG > $OUT/stdout 2> $OUT/stderr         && echo "KO -> exit status $? instead of 1"                              && exit 1
+! cmp -s $OUT/usage $OUT/stderr            && echo "KO -> unexpected output on stderr => check file \"$OUT/usage\"" && exit 1
+[ -s $OUT/stdout ]                         && echo "KO -> output detected on stdout"                                && exit 1
+
+$PROG a > $OUT/stdout 2> $OUT/stderr       && echo "KO -> exit status $? instead of 1"                              && exit 1
+! cmp -s $OUT/usage $OUT/stderr            && echo "KO -> unexpected output on stderr => check file \"$OUT/usage\"" && exit 1
+[ -s $OUT/stdout ]                         && echo "KO -> output detected on stdout"                                && exit 1
+
+$PROG a b c > $OUT/stdout 2> $OUT/stderr     && echo "KO -> exit status $? instead of 1"                              && exit 1
+! cmp -s $OUT/usage $OUT/stderr            && echo "KO -> unexpected output on stderr => check file \"$OUT/usage\"" && exit 1
+[ -s $OUT/stdout ]                         && echo "KO -> output detected on stdout"                                && exit 1
+
+echo "...............OK"
+
+######################################
+
+echo -n "test 02 - no dynamic memory allocation: "
+
+grep -q "[cm]alloc" $FILE && echo "KO -> dynamic memory allocation is not allowed!" && exit 1
+
+echo "OK"
+
+######################################
+
+echo -n "test 03 - invalid port number: "
+
+$PROG a 65001 > $OUT/stdout 2> $OUT/stderr && echo "KO -> exit status $? instead of 1" && exit 1
+! [ -s $OUT/stderr ]                       && echo "KO -> no output on stderr"         && exit 1
+[ -s $OUT/stdout ]                         && echo "KO -> output detected on stdout"   && exit 1
+
+$PROG a 9999 > $OUT/stdout 2> $OUT/stderr  && echo "KO -> exit status $? instead of 1" && exit 1
+! [ -s $OUT/stderr ]                       && echo "KO -> no output on stderr"         && exit 1
+[ -s $OUT/stdout ]                         && echo "KO -> output detected on stdout"   && exit 1
+
+echo ".........OK"
+
+######################################
+
+echo -n "test 04 - error on getaddrinfo: "
+
+nc fdjkfslkfj 80 > /dev/null 2> $OUT/error
+ERROR=$(grep -o '\S*$' $OUT/error)
+
+$PROG fdjkfslkfj $PORT > $OUT/stdout 2> $OUT/stderr && echo "KO -> exit status $?"            && exit 1
+[ -s $OUT/stdout ]                                  && echo "KO -> output detected on stdout" && exit 1
+
+STUDENT=$(grep -o '\S*$' $OUT/stderr)
+[ "$ERROR" != "$STUDENT" ] && echo "KO -> unexpected output on stderr, use gai_strerror whenever getaddrinfo fails" && exit 1
+
+echo "........OK"
+
+######################################
+
+echo -n "test 05 - program exits without error: "
+
+timeout 10 $PROG $IP $PORT > $OUT/stdout 2> $OUT/stderr &
 TO=$!
-sleep 1
-timeout 1 nc -6 ::1 $PORT < receiver-tcp &
+sleep 5
+timeout 1 nc -6 $IP $PORT < receiver-tcp &
 wait $!
 wait $TO
 R=$?
-[ "$R" == "124" ] && echo "KO -> program times out"           && exit 1
-[ "$R" != "0" ]   && echo "KO -> exit status $R instead of 0" && exit 1
-echo "............OK"
 
-echo -n "test 04 - file copy.tmp exists: "
+[ "$R" == "124" ]  && echo "KO -> program times out"           && exit 1
+[ "$R" != "0" ]    && echo "KO -> exit status $R instead of 0" && exit 1
+[ -s $OUT/stderr ] && echo "KO -> output detected on stdout"   && exit 1
+[ -s $OUT/stderr ] && echo "KO -> output detected on stderr"   && exit 1
+
+echo ".OK"
+
+######################################
+
+echo -n "test 06 - file copy.tmp exists: "
+
 [ ! -f "copy.tmp" ] && echo "KO -> file does not exist" && exit 1
-echo "...................OK"
 
-echo -n "test 05 - files are the same: "
-cmp receiver-tcp copy.tmp 2> /dev/null || (echo "KO -> files differ" && exit 1)
-echo ".....................OK"
+echo "........OK"
 
-echo -n "test 06 - check bind failure: "
+######################################
+
+echo -n "test 07 - files are the same: "
+
+! cmp -s receiver-tcp copy.tmp 2> /dev/null && echo "KO -> files differ" && exit 1
+
+echo "..........OK"
+
+######################################
+
+echo -n "test 08 - check bind failure: "
+
 timeout 5 nc -6l ::1 $PORT &
 TO=$!
 sleep 1
-$PROG $PORT 2> /dev/null && echo "KO -> exit status $? instead of 1" && exit 1 
-wait $TO
-echo ".....................OK"
 
-echo -n "test 07 - check open failure: "
+$PROG $IP $PORT > $OUT/stdout 2> $OUT/stderr && echo "KO -> exit status $? instead of 1" && exit 1 
+wait $TO
+[ -s $OUT/stdout ]   && echo "KO -> output detected on stdout"     && exit 1
+! [ -s $OUT/stderr ] && echo "KO -> no output detected on stderr"  && exit 1
+
+echo "..........OK"
+
+######################################
+
+echo -n "test 09 - check open failure: "
+
 chmod 000 copy.tmp
-$PROG $PORT 2> /dev/null &
+$PROG $IP $PORT > $OUT/stdout 2> $OUT/stderr &
 PID=$!
-timeout 1 nc -6 ::1 $PORT < receiver-tcp &
-wait $!
+sleep 5
+timeout 1 nc -6 $IP $PORT < receiver-tcp
 wait $PID
 R=$?
-[ "$R" != "1" ] && echo "KO -> exit status $R instead of 1" && exit 1
-chmod 644 copy.tmp
-echo ".....................OK"
 
-echo -n "test 08 - memory error: "
+[ "$R" != "1" ]      && echo "KO -> exit status $R instead of 1"  && exit 1
+[ -s $OUT/stdout ]   && echo "KO -> output detected on stdout"    && exit 1
+! [ -s $OUT/stderr ] && echo "KO -> no output detected on stderr" && exit 1
+
+chmod 644 copy.tmp
+
+echo "..........OK"
+
+######################################
+
+echo -n "test 10 - memory error: "
+
 P=`which valgrind`
 [ -z "$P" ] && echo "KO -> please install valgrind" && exit 1
-valgrind --leak-check=full --error-exitcode=100 --log-file=valgrind.log $PROG $PORT > /dev/null &
+
+valgrind --leak-check=full --error-exitcode=100 --log-file=$OUT/valgrind.log $PROG $IP $PORT > /dev/null &
 V=$!
-sleep 2
+sleep 5
 cat receiver-tcp | nc -6 -w1 ::1 $PORT
 wait $V
 [ "$?" == "100" ] && echo "KO -> memory pb please check valgrind.log" && exit 1
-echo "...........................OK"
 
-rm copy.tmp valgrind.log
+echo "................OK"
+
+######################################
+
+rm -r $OUT
